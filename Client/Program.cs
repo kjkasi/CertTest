260e7314d40a08ce9f4680d7d950dc861b31288b
kjkasi <5842602+kjkasi@users.noreply.github.com>
2022-10-31 19:46:53 +0300

    Add Client project

---
+++ Client/Program.cs
@@ -0,0 +1,10 @@
+﻿// discover endpoints from metadata
+using IdentityModel.Client;
+
+var client = new HttpClient();
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    return;
+}
